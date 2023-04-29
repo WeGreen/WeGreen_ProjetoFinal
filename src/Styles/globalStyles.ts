7674b63fb25fa056dacd44356dc6ce4436b1a3d0
Ana Luiza <alclim02@gmail.com>
2023-04-29 11:39:58 -0400

    style: adds global styles file, adds color variables, font link added to index.html

---
+++ src/Styles/globalStyles.ts
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components"
+
+export const GlobalStyle = createGlobalStyle`
+    :root {
+        --green1: #45710F;
+        --green2: #61844AE5;
+        --purple1: ##9449AE;
+        --purple2: ##AF98B6;
+        --black: #202120;
+        --grey1: #90948F;
+        --grey2: ##F5F5F5;
+        --white: #FFFFFF;
+    }
+
+`
