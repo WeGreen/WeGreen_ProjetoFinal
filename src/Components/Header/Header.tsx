782727c1fdd9260e1727c5552e149f6f2f81f9be
Jdel-Campos <campos.34pss@gmail.com>
2023-05-05 09:44:03 -0300

    fix: image correction

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,7 +1,9 @@
 import { StyledHeaderContainer } from "./HeaderStyle";
 import { useContext } from "react";
 import { UserContext } from "../../Providers/UserContext";
-
+import Logo_img from "../../assets/logo.svg"
+import Logout_img from "../../assets/logout.svg"
+ 
 export const Header = () => {
     const { logout } = useContext(UserContext);
 
@@ -9,11 +11,11 @@ export const Header = () => {
         <StyledHeaderContainer>
             <div className="header_innerContainer">
                 <div className="title_container">
-                    <img className="logo_img" src="src/assets/logo.svg" alt="WeGreen logo" />
+                    <img className="logo_img" src={Logo_img} alt="WeGreen logo" />
                     <h1 className="header_title">WeGreen</h1>
                 </div>
                 <button className="button_logout" onClick={logout}>
-                    <img className="img_logout" src="src/assets/logout.svg" alt="Sair do perfil"/>
+                    <img className="img_logout" src={Logout_img} alt="Sair do perfil"/>
                 </button>
             </div>  
         </StyledHeaderContainer>
