29b37efe8274546ecb5ce4d11ca0a10984fc0481
Gabriela <gabiolmelo@gmail.com>
2023-05-05 16:00:10 -0300

    fix: modal click

--- src/Components/PostsList/PostsList.tsx
+++ src/Components/PostsList/PostsList.tsx
@@ -1,10 +1,17 @@
-import { useContext } from "react";
-import { PostContext } from "../../Providers/PostContext"; 
+import { useState, useContext } from "react";
+import { PostContext } from "../../Providers/PostContext";
+import { DeletePostModal } from "../Modal/DeletePostModal/DeletePostModal";
+import { TPost } from "../../Utilities/api";
 
 export const PostsList = () => {
   const { postsList } = useContext(PostContext);
-  console.log(postsList);
-  
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+  const [selectedPost, setSelectedPost] = useState<TPost | null>(null);
+
+  const handleDeleteClick = (post: TPost) => {
+    setSelectedPost(post);
+    setIsDeleteModalOpen(true);
+  };
 
   return (
     <div>
@@ -12,8 +19,17 @@ export const PostsList = () => {
         <div key={post.id}>
           <h2>{post.title}</h2>
           <p>{post.content}</p>
+          <div className="BtnsContainer">
+            <button onClick={() => handleDeleteClick(post)}>Excluir</button>
+            <button>Editar</button>
+          </div>
         </div>
       ))}
+      <DeletePostModal
+        isOpen={isDeleteModalOpen}
+        onClose={() => setIsDeleteModalOpen(false)}
+        post={selectedPost as TPost}
+      />
     </div>
   );
 };
